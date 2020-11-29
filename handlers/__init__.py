ba8f73eb544c4999baca735a73044fdd78f9782a
dzenbots <vv.khritkin@gmail.com>
2020-11-29 15:06:11 +0300

    some fixes

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,4 @@
 from .errors import dp
-from .users import dp, check_valid_tuser, is_valid_user, is_private
 
 
-__all__ = ["dp", "check_valid_tuser", "is_valid_user", "is_private"]
+__all__ = ["dp"]
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -3,9 +3,8 @@ from .start import dp
 from .groups import dp
 from .tusers_callbacks import dp
 from .groups_callbacks import dp
-from utils.help_functions import check_valid_tuser, is_valid_user, is_private
 from .users_info import dp
 from.main_inline_callbacks import dp
 from .row_text_messages import dp, show_main_menu
 
-__all__ = ["dp", "check_valid_tuser", "is_valid_user", "is_private", "show_main_menu"]
+__all__ = ["dp"]
